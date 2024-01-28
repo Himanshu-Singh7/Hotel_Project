f0a05e0156dac258f8769cd71e13346709af9e0f
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2024-01-28 17:32:22 +0530

    Add files via upload

--- userService/src/test/java/com/user/service/UserServiceApplicationTests.java
+++ userService/src/test/java/com/user/service/UserServiceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.user.service;
 
+import com.user.service.Entites.Rating;
+import com.user.service.External.Services.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +13,16 @@ class UserServiceApplicationTests {
 	void contextLoads() {
 	}
 
+
+	@Autowired
+	private RatingService ratingService;
+	// Test that the services
+//	@Test
+//	void createRating(){
+//		Rating rating = Rating.builder().rating(10).userId("").hotelId("").feedback("this is creating using feign client").build();
+//
+//		Rating saveRating = ratingService.createRating(rating);
+//
+//		System.out.println("new rating created successfully" + saveRating.toString());
+//	}
 }
